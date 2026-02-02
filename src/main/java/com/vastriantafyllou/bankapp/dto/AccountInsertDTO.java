f6fec8b25f20d7f714d9fcd181c7c974a0c616fa
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2026-02-02 14:27:11 +0200

    feat(dto): update validation for IBAN and Account Number fields in AccountInsertDTO

--- src/main/java/com/vastriantafyllou/bankapp/dto/AccountInsertDTO.java
+++ src/main/java/com/vastriantafyllou/bankapp/dto/AccountInsertDTO.java
@@ -3,7 +3,7 @@ package com.vastriantafyllou.bankapp.dto;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,11 +18,11 @@ import java.math.BigDecimal;
 public class AccountInsertDTO {
 
     @NotBlank(message = "Το IBAN είναι υποχρεωτικό")
-    @Size(min = 5, message = "Το IBAN πρέπει να έχει τουλάχιστον 5 χαρακτήρες")
+    @Pattern(regexp = "^GR\\d{15}$", message = "Το IBAN πρέπει να ξεκινάει με GR και να ακολουθούν 15 αριθμοί")
     private String iban;
 
     @NotBlank(message = "Το Account Number είναι υποχρεωτικό")
-    @Size(min = 5, message = "Το Account Number πρέπει να έχει τουλάχιστον 5 χαρακτήρες")
+    @Pattern(regexp = "^\\d{10}$", message = "Το Account Number πρέπει να είναι ακριβώς 10 αριθμοί")
     private String accountNumber;
 
     @NotNull(message = "Το αρχικό υπόλοιπο είναι υποχρεωτικό")

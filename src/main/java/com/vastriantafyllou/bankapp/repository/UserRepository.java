4b7543840d4d93da5755e91603f6f3e933f326ae
vas.triantafyllou <triantafyllou.vasileios@gmail.com>
2026-02-13 11:21:46 +0200

    feat(repository): add count query to UserRepository

--- src/main/java/com/vastriantafyllou/bankapp/repository/UserRepository.java
+++ src/main/java/com/vastriantafyllou/bankapp/repository/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     Optional<AppUser> findByUsername(String username);
     boolean existsByUsername(String username);
+    long count();
 }
